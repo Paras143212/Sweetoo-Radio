4ae0c9c61b27882370ba52bb4d3973c94657e39a
Paras143212 <paraszgandhi@gmail.com>
2026-09-07 09:39:45 +0530

    Change CACHE_NAME to sweetoo-radio-v4
    Updated cache name to v4 for cache refresh.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sweetoo-radio-v3'; // Updated to v3 to force a cache refresh
+const CACHE_NAME = 'sweetoo-radio-v4'; // Updated to v4 to force a cache refresh
 const urlsToCache = [
   './index.html',
   './App image.png'
